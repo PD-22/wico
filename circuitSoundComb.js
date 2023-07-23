2a8dd85680aa2576c7dc3346410703b1218bd199
Pavle <p.devidze@gmail.com>
2023-07-23 12:46:45 +0400

    refactor naming

--- circuitSoundComb.js
+++ circuitSoundComb.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const { zip, countObjDiff, mapObject } = require('./utils');
 const { getMinDiffPermutations } = require('./permutationsOptimization');
 
-const auxPermutations = getOptimizedKeyValuePermutations(
+const auxPermutations = getMinDiffKeyValuePermutations(
     "mrlg".split(""),
     "blue red green copper".split(" ")
 );
 
-const soundPermutations = getOptimizedKeyValuePermutations(
+const soundPermutations = getMinDiffKeyValuePermutations(
     "rgl".split(""),
     "red copper green".split(" ")
 );
@@ -17,14 +17,14 @@ const wiringCombinations = combineArraysWithKeys(
     'sound', soundPermutations
 );
 
-const formattedCombinations = formatCircuitSoundCombinations(wiringCombinations);
+const formattedCombinations = formatWiringCombinations(wiringCombinations);
 fs.writeFileSync('output.txt', formattedCombinations);
 
 const oldFormattedCombinations = fs.readFileSync('output copy.txt', 'utf8');
 
 console.log(`file content matches: ${oldFormattedCombinations === formattedCombinations}`);
 
-function getOptimizedKeyValuePermutations(keys, values) {
+function getMinDiffKeyValuePermutations(keys, values) {
     return getMinDiffPermutations(values).map(valuesPermutation =>
         Object.fromEntries(zip(keys, valuesPermutation))
     );
@@ -36,20 +36,20 @@ function combineArraysWithKeys(key1, array1, key2, array2) {
     ));
 }
 
-function formatCircuitSoundCombinations(wiringCombinations) {
-    const enrichedCombination = wiringCombinations.map(enrichCombination);
+function formatWiringCombinations(wiringCombinations) {
+    const enrichedCombination = wiringCombinations.map(enrichWiringCombinations);
 
     const formattedCombinations = enrichedCombination.map(formatEnrichedCombination).join('');
-    const totalSwitches = enrichedCombination.map(x => x.diff).reduce((a, b) => a + b);
+    const totalDiffs = enrichedCombination.map(x => x.diff).reduce((a, b) => a + b);
 
-    return `total switches: ${totalSwitches}\n\nall combinations:\n${formattedCombinations}`;
+    return `total diffs: ${totalDiffs}\n\ncombinations:\n${formattedCombinations}`;
 
     function formatEnrichedCombination({ comb, diff }, i) {
         return `  #${i + 1} (${diff}):\n${circuitSoundCombToString(comb, '    ')}`;
     }
 }
 
-function enrichCombination(comb, i) {
+function enrichWiringCombinations(comb, i) {
     const nextComb = wiringCombinations[i + 1];
 
     const result = {
@@ -65,16 +65,16 @@ function enrichCombination(comb, i) {
         return mapObject(comb, (type, wires) => {
             const nextWires = nextComb?.[type];
 
-            const newWires = mapObject(wires, (label, color) => {
-                const newColor = { color };
+            const newWires = mapObject(wires, (label, wire) => {
+                const newWire = { value: wire };
 
-                const nextColor = nextWires?.[label];
-                const colorChanged = nextColor && nextColor !== color;
-                if (colorChanged) {
-                    newColor.next = nextColor;
-                    newColor.nextNumber = ++nextNumberCounter;
+                const nextWire = nextWires?.[label];
+                const wireChanged = nextWire && nextWire !== wire;
+                if (wireChanged) {
+                    newWire.next = nextWire;
+                    newWire.nextNumber = ++nextNumberCounter;
                 }
-                return [label, newColor];
+                return [label, newWire];
             });
 
             return [type, newWires];
@@ -97,8 +97,8 @@ function circuitSoundCombToString(comb, padding = '') {
 
     Object.keys(comb).forEach(wiringName => {
         result += `${padding}${wiringName}:\n`;
-        for (const [key, { color, next, nextNumber }] of Object.entries(comb[wiringName])) {
-            result += `${padding}  ${key} - ${color}`;
+        for (const [key, { value, next, nextNumber }] of Object.entries(comb[wiringName])) {
+            result += `${padding}  ${key} - ${value}`;
             if (next) result += ` -> ${next} (${nextNumber})`;
             result += "\n";
         }
