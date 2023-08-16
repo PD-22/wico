43c7645d34283df2841dc51b6c6ae221793d44b8
Pavle <p.devidze@gmail.com>
2023-08-16 08:54:45 +0400

    refactor getCombinations generator

--- combinations.js
+++ combinations.js
@@ -1,15 +1,25 @@
 const { product, indices } = require("./utils");
 
 function getCombinations(arrays) {
-    const resultLength = product(arrays.map(x => x.length));
-    return indices(resultLength).map(index => {
-        return indices(arrays.length).map(arrayIndex => {
-            const currArray = arrays[arrayIndex];
-            const l1 = currArray.length;
-            const l2 = product(arrays.slice(arrayIndex).map(x => x.length));
-            return currArray[Math.floor(l1 / l2 * index) % l1];
-        })
-    });
+    return Array.from(getCombinationsGenerator(arrays));
+}
+
+function* getCombinationsGenerator(arrays) {
+    for (let i = 0; i < getCombinationsLength(arrays); i++)
+        yield getCombinationAtIndex(arrays, i);
+}
+
+function getCombinationsLength(arrays) {
+    return product(arrays.map(x => x.length));
+}
+
+function getCombinationAtIndex(arrays, index) {
+    return indices(arrays.length).map(arrayIndex => {
+        const currArray = arrays[arrayIndex];
+        const l1 = currArray.length;
+        const l2 = product(arrays.slice(arrayIndex).map(x => x.length));
+        return currArray[Math.floor(l1 / l2 * index) % l1];
+    })
 }
 
 function getMinDiffCombinations(arrays) {
