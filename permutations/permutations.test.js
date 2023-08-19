ea2af4beac999a18c422038b684f69de307a6170
Pavle <p.devidze@gmail.com>
2023-08-19 19:21:12 +0400

    extract permutation utils

--- permutations/permutations.test.js
+++ permutations/permutations.test.js
@@ -1,37 +1,9 @@
-const { getPermutationsLength } = require('./permutations');
-const { getMinDiffPermutationsGenerator } = require('./permutationsOptimization');
-const { createProgressBar, logDeltaTimeAsync } = require('../utils/debug');
-const { writeGenerator, enrichGenerator } = require('../utils/generator');
-const { createCharSequence, countListDiff } = require("../utils/general");
+const { createCharSequence } = require("../utils/general");
 const path = require('path');
+const { testPermutations } = require("./utils");
 
 testPermutations(
     createCharSequence('A', 9),
     path.join(__dirname, 'output.txt'),
     x => x.join('') + '\n'
 );
-
-async function testPermutations(set, outputFile, formatEntry = x => x.join(' ') + '\n') {
-    console.log(`input: ${JSON.stringify(set)}`);
-
-    const generator = enrichGenerator(getMinDiffPermutationsGenerator(set))
-        .forEach(createCheckMinDiffPermutations())
-        .forEach(createProgressBar(getPermutationsLength(set.length), 30).increment)
-        .map(formatEntry);
-
-    try {
-        await logDeltaTimeAsync(writeGenerator)(outputFile, generator);
-        console.log(`result: ${outputFile}`);
-    } catch (error) {
-        console.error(`An error occurred while writing to ${outputFile}: ${error}`);
-    }
-}
-
-function createCheckMinDiffPermutations() {
-    let prevValue = null;
-    return value => {
-        const isInvalid = prevValue && countListDiff(prevValue, value) !== 2;
-        if (isInvalid) throw new Error('Invalid adjacent permutations encountered');
-        prevValue = value;
-    }
-}
