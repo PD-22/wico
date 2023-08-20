87c86a700d727ced8c9757ffb2170718c295ac1a
Pavle <p.devidze@gmail.com>
2023-08-20 23:34:21 +0400

    refactor combinationsCompare.test, fix typo

--- combinations-old/combinationsCompare.test.js
+++ combinations-old/combinationsCompare.test.js
@@ -7,19 +7,17 @@ const path = require('path');
 
 const commonRange = range(2, 50);
 const testInputs1 = [Array(2).fill(0), commonRange];
-// const testInputs2 = [Array(3).fill(0), commonRange];
-// const testInputs = [testInputs1, testInputs2];
-const testInputs = [testInputs1];
+const testInputs2 = [Array(3).fill(0), commonRange];
+const testInputs = [testInputs1, testInputs2];
 
+const outputFileNewCombinations = path.join(__dirname, 'output-new-combinations.txt');
 console.log('Measure new combinations method...');
-const [deltaTimeNew, newCombinationsResult] = getDeltaTime(() => testCharCombinations({ testInputs }));
+const [deltaTimeNew, newCombinationsResult] = getDeltaTime(() => testCharCombinations(testInputs));
 console.log(`New delta time: ${deltaTimeNew.toFixed(2)}`);
 
+const outputFileOldCombinations = path.join(__dirname, 'output-old-combinations.txt');
 console.log('Measure old combinations method...');
-const [deltaTimeOld, oldCombinationsResult] = getDeltaTime(() => testCharCombinations({
-    testInputs,
-    getCombinationsCallback: getCombinationsOld
-}));
+const [deltaTimeOld, oldCombinationsResult] = getDeltaTime(() => testCharCombinations(testInputs, getCombinationsOld));
 console.log(`Old delta time: ${deltaTimeOld.toFixed(2)}`);
 
 const total = deltaTimeNew + deltaTimeOld;
@@ -28,8 +26,7 @@ const diff = deltaTimeNew - deltaTimeOld;
 console.log(`Diff delta time (new - old): ${diff.toFixed(2)}`);
 console.log(`Total / diff: ${(100 * diff / total).toFixed(2)}%`);
 
-const outputFileNewCombinations = path.join(__dirname, 'output-old-combinations.txt');
-const outputFileOldCombinations = path.join(__dirname, 'output-new-combinations.txt');
+
 console.log(`Writing results to: "${outputFileOldCombinations}" and "${outputFileNewCombinations}"`);
 const resultsMatch = deepArrayCompare(newCombinationsResult, oldCombinationsResult);
 if (!resultsMatch) console.error('Different combination results!');
@@ -37,7 +34,7 @@ fs.writeFileSync(outputFileNewCombinations, formatCombinations(newCombinationsRe
 fs.writeFileSync(outputFileOldCombinations, formatCombinations(oldCombinationsResult));
 console.log('Done');
 
-function testCharCombinations({ testInputs, getCombinationsCallback }) {
+function testCharCombinations(testInputs, getCombinationsCallback) {
     return testInputs.flatMap(testInput => {
         const [firstNums, possibleLengths] = testInput;
         return getNumSequenceVariants(firstNums, possibleLengths, getCombinationsCallback);
@@ -45,5 +42,5 @@ function testCharCombinations({ testInputs, getCombinationsCallback }) {
 }
 
 function formatCombinations(combinations) {
-    return combinations.map(x => x.map(x => x.join(' ')).join('\n')).join('\n\n')
+    return combinations.map(x => x.map(x => x.join(' ')).join(' | ')).join('\n')
 }
