be01876d2ed8dc2948c10a68af21be765b3b723b
Pavle <p.devidze@gmail.com>
2023-08-25 16:11:01 +0400

    refactor combinationsCompare.test.js
    update console output
    use testing util functions
    refactor
    extract code groups into functions

--- combinations-old/combinationsCompare.test.js
+++ combinations-old/combinationsCompare.test.js
@@ -1,52 +1,43 @@
 const fs = require('fs');
 const path = require('path');
-const { range, deepArrayCompare } = require('../utils/general');
+const { range, } = require('../utils/general');
 const { getNumSequenceVariants } = require('../combinations/utils');
-const { getDeltaTime } = require('../utils/debug');
+const { getDeltaTime, comparePerfomance } = require('../utils/debug');
 const { getCombinationsOld } = require('./combinationsOld');
+const { getCombinations } = require('../combinations/combinations');
+const { checkResultsMatch } = require('./utils');
 
-const testInputs = range(2, 3).map(x => ({
-    firstNums: Array(x).fill(0),
-    possibleLengths: range(2, 50)
-}));
+const possibleLengths = range(2, 50);
+const testInputs = range(2, 3).map(x => ({ firstNums: Array(x).fill(0), possibleLengths }));
 
-const outputFileNewCombinations = path.join(__dirname, 'output-new-combinations.txt');
-const outputFileOldCombinations = path.join(__dirname, 'output-old-combinations.txt');
+const [deltaTimeNew, newCombinationsResult] = measurePerfomance(getCombinations, testInputs);
+const [deltaTimeOld, oldCombinationsResult] = measurePerfomance(getCombinationsOld, testInputs);
 
-console.log('Measure new combinations method...');
-const [deltaTimeNew, newCombinationsResult] = getDeltaTime(() => testCharCombinations(testInputs));
-console.log(`New delta time: ${deltaTimeNew.toFixed(2)}`);
+comparePerfomance(deltaTimeNew, deltaTimeOld);
 
-console.log('Measure old combinations method...');
-const [deltaTimeOld, oldCombinationsResult] = getDeltaTime(() => testCharCombinations(testInputs, getCombinationsOld));
-console.log(`Old delta time: ${deltaTimeOld.toFixed(2)}`);
+checkResultsMatch(newCombinationsResult, oldCombinationsResult);
 
-const total = deltaTimeNew + deltaTimeOld;
-const diff = deltaTimeNew - deltaTimeOld;
+writeResult(newCombinationsResult, path.join(__dirname, 'output-new-combinations.txt'));
+writeResult(oldCombinationsResult, path.join(__dirname, 'output-old-combinations.txt'));
 
-console.log(`Total delta time: ${total.toFixed(2)}`);
-console.log(`Diff delta time (new - old): ${diff.toFixed(2)}`);
-console.log(`Total / diff: ${(100 * diff / total).toFixed(2)}%`);
-
-console.log(`Writing results to: "${outputFileOldCombinations}" and "${outputFileNewCombinations}"`);
-const resultsMatch = deepArrayCompare(newCombinationsResult, oldCombinationsResult);
-
-if (resultsMatch) {
-    console.log('Combination results match');
-} else {
-    console.error('Different combination results!');
-}
-
-fs.writeFileSync(outputFileNewCombinations, formatCombinations(newCombinationsResult));
-fs.writeFileSync(outputFileOldCombinations, formatCombinations(oldCombinationsResult));
-console.log('Done');
-
-function testCharCombinations(testInputs, getCombinationsCallback) {
-    return testInputs.flatMap(({ firstNums, possibleLengths }) =>
-        getNumSequenceVariants(firstNums, possibleLengths, getCombinationsCallback)
+function measurePerfomance(methodCallback, testInputs) {
+    console.log(`${methodCallback.name}...`);
+    const [deltaTime, result] = getDeltaTime(() =>
+        testInputs.flatMap(({ firstNums, possibleLengths }) =>
+            getNumSequenceVariants(firstNums, possibleLengths, methodCallback)
+        )
     );
+    console.log(`${deltaTime.toFixed(2)} ms\n`);
+
+    return [deltaTime, result];
 }
 
-function formatCombinations(combinations) {
-    return combinations.map(sequence => sequence.map(item => item.join(' ')).join(' | ')).join('\n');
+function writeResult(result, file) {
+    console.log(`Writing to "${file}"...`);
+    fs.writeFileSync(file, formatCombinations(result));
+    console.log('Done\n');
+
+    function formatCombinations(combinations) {
+        return combinations.map(sequence => sequence.map(item => item.join(' ')).join(' | ')).join('\n');
+    }
 }
