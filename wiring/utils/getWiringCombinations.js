211922e995d5b2e32f44258312bea0163d65cb24
Pavle <p.devidze@gmail.com>
2023-08-26 16:10:16 +0400

    update getWiringCombinations
    make it work on multiple wiring settings

--- wiring/utils/getWiringCombinations.js
+++ wiring/utils/getWiringCombinations.js
@@ -1,17 +1,11 @@
 import { getMinDiffCombinations } from "../../combinations/combinationsOptimization.js";
 import { getMinDiffPermutations } from "../../permutations/permutationsOptimization.js";
-import { zip } from "../../utils/general.js";
+import { transformObject, zip } from "../../utils/general.js";
 
 export default function getWiringCombinations(wiringSettings) {
-    const [settings1, settings2] = wiringSettings;
-
-    const permutations1 = getWiringPermutations(settings1);
-    const permutations2 = getWiringPermutations(settings2);
-
-    return getMinDiffCombinations({
-        [settings1.name]: permutations1,
-        [settings2.name]: permutations2
-    });
+    return getMinDiffCombinations(transformObject(wiringSettings,
+        (k, settings) => [settings.name, getWiringPermutations(settings)]
+    ));
 }
 
 function getWiringPermutations(wiringSetting) {
