46fbfb874742f102202624e73520edc5bc7e04b4
Pavle <p.devidze@gmail.com>
2023-10-09 06:23:22 +0400

    prepare new combinations

---
+++ combinations/combinationsNew.js
@@ -0,0 +1,31 @@
+import { combineDict } from "../utils/combinatorics.js";
+import { product } from "../utils/general.js";
+
+export function getKeyValueCombinations(arrays) {
+    return combineDict(arrays, getCombinations);
+}
+
+export default function getCombinations(arrays) {
+    return Array.from(getCombinationsGenerator(arrays));
+}
+
+export function* getCombinationsGenerator(arrays) {
+    const combinationsLength = getCombinationsLength(arrays);
+    for (let i = 0; i < combinationsLength; i++)
+        yield getCombinationAtIndex(arrays, i);
+}
+
+export function getCombinationsLength(arrays) {
+    return product(Object.values(arrays).map(x => x.length));
+}
+
+export function getCombinationAtIndex(arrays, index) {
+    let l2 = product(Object.values(arrays).map(x => x.length));
+    return arrays.map(currArray => {
+        const l1 = currArray.length;
+        // NOTE: division should be perfomed last to avoid floating-point rounding Errors
+        const resultIndex = Math.floor(index * l1 / l2) % l1;
+        l2 /= l1;
+        return currArray[resultIndex];
+    });
+}
