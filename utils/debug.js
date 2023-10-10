faf7f33a83b042f5e6ee202cb083b407612b10f9
Pavle <p.devidze@gmail.com>
2023-10-10 15:22:21 +0400

    update comparePerfomance relDiff

--- utils/debug.js
+++ utils/debug.js
@@ -88,11 +88,11 @@ export async function compareDataToFile(data, compareFile) {
 export function comparePerfomance(time1, time2) {
     const total = time1 + time2;
     const diff = time1 - time2;
-    const relDiff = Math.sign(diff) * 100 * 2 * Math.abs(diff) / total;
+    const relDiff = Math.sign(diff) * Math.abs(diff) / total;
 
     console.log(`Total: ${total.toFixed(2)} ms`);
     console.log(`Diff: ${diff.toFixed(2)} ms`);
-    console.log(`Relative diff: ${relDiff.toFixed(2)}%\n`);
+    console.log(`Relative diff: ${(100 * relDiff).toFixed(2)}%\n`);
 }
 
 export function getDirname(importMetaUrl) {
