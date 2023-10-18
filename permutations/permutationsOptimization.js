22373e9f162dfc6dad0b3e845984760736ba684d
Pavle <p.devidze@gmail.com>
2023-10-18 09:04:11 +0400

    revert permutationsOptimization module version

--- permutations/permutationsOptimization.js
+++ permutations/permutationsOptimization.js
@@ -1,46 +1,38 @@
 import combineDict from "../utils/combineDict.js";
-import { factorial } from "../utils/general.js";
-
-export default function getMinDiffPermutations(set) {
-    const resultList = [];
-
-    const permutationsLength = factorial(set.length);
-    for (let index = 0; index < permutationsLength; index++) {
-        let groupSize = set.length;
-        let height = permutationsLength / groupSize--;
-        let offset = 0;
-
-        let cacheA = index;
-        while (groupSize > 1) {
-            const leftover = Math.floor(cacheA / height);
-            const isOdd = leftover % 2 === 1;
-            offset += leftover * height;
-            cacheA %= height;
-            if (isOdd) cacheA = height - cacheA - 1;
-            height /= groupSize--;
-        }
-
-        const minDiffPermutationSwapIndex = cacheA + offset;
-
-        const remainingSet = set.slice();
-        const result = [];
-
-        let cacheB = minDiffPermutationSwapIndex;
-        for (let j = set.length - 1; j >= 0; j--) {
-            const quotient = Math.floor(cacheB / factorial(j));
-
-            result.push(remainingSet[quotient]);
-            remainingSet.splice(quotient, 1);
-
-            cacheB %= factorial(j);
-        }
-
-        resultList[index] = result;
-    }
-
-    return resultList;
-}
+import { getPermutationAtIndex, getPermutationsLength } from "./permutations.js";
 
 export function getMinDiffKeyValuePermutations(set) {
     return combineDict(set, getMinDiffPermutations);
+}
+
+export default function getMinDiffPermutations(set) {
+    return Array.from(getMinDiffPermutationsGenerator(set));
+}
+
+export function* getMinDiffPermutationsGenerator(set) {
+    const totalLength = getPermutationsLength(set.length);
+    for (let index = 0; index < totalLength; index++)
+        yield getMinDiffPermutationAtIndex(set, index, totalLength);
+}
+
+export function getMinDiffPermutationAtIndex(set, index, totalLength = getPermutationsLength(set)) {
+    return getPermutationAtIndex(set, getMinDiffPermutationSwapIndex(set.length, index, totalLength));
+}
+
+export function getMinDiffPermutationSwapIndex(length, index, totalLength = getPermutationsLength(set)) {
+    if (index < 0 || index >= totalLength) throw new RangeError();
+
+    let height = totalLength / length--;
+    let offset = 0;
+
+    while (length > 1) {
+        const leftover = Math.floor(index / height);
+        const isOdd = leftover % 2 === 1;
+        offset += leftover * height;
+        index %= height;
+        if (isOdd) index = height - index - 1;
+        height /= length--;
+    }
+
+    return index + offset;
 }
