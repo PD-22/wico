3d0ea97347c7bc69026c24aab3facbfd04b5189f
Pavle <p.devidze@gmail.com>
2023-10-18 10:05:09 +0400

    revert combinations module version

--- combinations/combinations.js
+++ combinations/combinations.js
@@ -1,24 +1,54 @@
 import combineDict from "../utils/combineDict.js";
 import { product } from "../utils/general.js";
 
-export default function getCombinations(arrays) {
-    const result = [];
-    const amount = product(arrays.map(x => x.length));
-
-    for (let i = 0; i < amount; i++) {
-        let groupSize = amount;
-        result.push(arrays.map(array => {
-            const l = array.length;
-            // NOTE: division should be performed last to avoid floating-point rounding errors
-            const resultIndex = Math.floor(i * l / groupSize) % l;
-            groupSize /= l;
-            return array[resultIndex];
-        }));
-    }
-
-    return result;
-}
-
 export function getKeyValueCombinations(arrays) {
     return combineDict(arrays, getCombinations);
+}
+
+export default function getCombinations(arrays) {
+    return Array.from(getCombinationsGenerator(arrays));
+}
+
+export function* getCombinationsGenerator(arrays) {
+    const combinationsLength = getCombinationsLength(arrays);
+    for (let i = 0; i < combinationsLength; i++)
+        yield getCombinationAtIndex(arrays, i, combinationsLength);
+}
+
+export function getCombinationsLength(arrays) {
+    return product(arrays.map(x => x.length));
+}
+
+export function getCombinationAtIndex(
+    arrays, combIndex,
+    combinationsLength = getCombinationsLength(arrays)
+) {
+    let groupSizeCache = combinationsLength;
+    return arrays.map((array, itemIndex) => {
+        const result = getCombinationItem(arrays, combIndex, itemIndex, groupSizeCache);
+        groupSizeCache /= array.length;
+        return result;
+    });
+}
+
+export function getCombinationItem(
+    arrays, combIndex, itemIndex,
+    groupSize = getGroupSize(arrays, itemIndex)
+) {
+    const array = arrays[itemIndex];
+    const resultIndex = getCombinationItemIndex(arrays, combIndex, itemIndex, groupSize);
+    return array[resultIndex];
+}
+
+export function getCombinationItemIndex(
+    arrays, combIndex, itemIndex,
+    groupSize = getGroupSize(arrays, itemIndex)
+) {
+    const array = arrays[itemIndex];
+    // NOTE: division should be performed last to avoid floating-point rounding errors
+    return Math.floor(combIndex * array.length / groupSize) % array.length;
+}
+
+export function getGroupSize(arrays, itemIndex) {
+    return getCombinationsLength(arrays.slice(itemIndex));
 }
