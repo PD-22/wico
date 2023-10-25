730bd6dede22039adc2dcc1d724cda4108e61478
Pavle <p.devidze@gmail.com>
2023-10-25 10:17:01 +0400

    refactor combineDict

--- utils/combineDict.js
+++ utils/combineDict.js
@@ -1,12 +1,8 @@
 import createObject from "../utils/createObject.js";
+import unzip from "./unzip.js";
 
 // NOTE: combinations dictionary support util
 export default function combineDict(dictionary, getValuesList) {
-    const values = Object.values(dictionary);
-    const valuesList = getValuesList(values);
-
-    return valuesList.map(newValues => {
-        const keys = Object.keys(dictionary);
-        return createObject(keys, newValues);
-    });
+    const [keys, values] = unzip(Object.entries(dictionary));
+    return getValuesList(values).map(newValues => createObject(keys, newValues));
 }
