ddb08270de45e5e808364fabc0b320815cf51fe3
Pavle <p.devidze@gmail.com>
2023-10-30 10:57:44 +0400

    describe combinatorics

---
+++ combinations/combinations.test.js
@@ -0,0 +1,33 @@
+import getCombinations from "./combinations.js";
+
+const combinations = getCombinations([[1, 2, 3], ['a', 'b', 'c'], ['A', 'B', 'C']]);
+console.log(combinations.map(arr => arr.join(' ')).join('\n'));
+/*
+1 a A
+1 a B
+1 a C
+1 b A
+1 b B
+1 b C
+1 c A
+1 c B
+1 c C
+2 a A
+2 a B
+2 a C
+2 b A
+2 b B
+2 b C
+2 c A
+2 c B
+2 c C
+3 a A
+3 a B
+3 a C
+3 b A
+3 b B
+3 b C
+3 c A
+3 c B
+3 c C
+*/
---
+++ permutations/permutationsOptimization.test.js
@@ -0,0 +1,31 @@
+import getMinDiffPermutations from "./permutationsOptimization.js";
+
+const permutations = getMinDiffPermutations([1, 2, 3, 4]);
+console.log(permutations.map(arr => arr.join(' ')).join('\n'));
+/*
+1 2 3 4
+1 2 4 3
+1 3 4 2
+1 3 2 4
+1 4 2 3
+1 4 3 2
+2 4 3 1
+2 4 1 3
+2 3 1 4
+2 3 4 1
+2 1 4 3
+2 1 3 4
+3 1 2 4
+3 1 4 2
+3 2 4 1
+3 2 1 4
+3 4 1 2
+3 4 2 1
+4 3 2 1
+4 3 1 2
+4 2 1 3
+4 2 3 1
+4 1 3 2
+4 1 2 3
+*/
+// difference between every adjacent permutation is only 2
