db77814c7755d76b67b5db057b552456a2d9a368
Pavle <p.devidze@gmail.com>
2023-10-30 10:29:48 +0400

    describe getDirname

--- debug/getDirname.js
+++ debug/getDirname.js
@@ -1,6 +1,8 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+// returns location of current file
+// requires import.meta.url of the file
 export default function getDirname(importMetaUrl) {
     return dirname(fileURLToPath(importMetaUrl));
 }
---
+++ debug/getDirname.test.js
@@ -0,0 +1,4 @@
+import getDirname from "./getDirname.js";
+
+console.log(getDirname(import.meta.url));
+// ...\wico\debug
