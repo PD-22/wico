674dabda5db50ae38d6abb694c0fbf387653269b
Pavle <p.devidze@gmail.com>
2023-11-26 10:39:17 +0400

    fix range util default param bug

--- utils/range.js
+++ utils/range.js
@@ -1,13 +1,13 @@
 /**
- * @param {number} end 
  * @param {number} start 
- * @param {number} step 
+ * @param {number} end 
+ * @param {number} step
  * @returns {number[]}
  * 
  * @example
  * range(0, 10, 2); // [0, 2, 4, 6, 8, 10]
  */
-export default function range(start, end, step) {
+export default function range(start, end, step = 1) {
     const result = [];
     for (let i = start; i <= end; i += step) result.push(i);
     return result
