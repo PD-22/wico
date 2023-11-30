a65ba342df67d9464ad9b4f22dae54760f12b2c4
Pavle <p.devidze@gmail.com>
2023-11-30 13:46:50 +0400

    inline wiring test

--- wiring/wiring.test.js
+++ wiring/wiring.test.js
@@ -1,23 +1,25 @@
 import { join } from "path";
 import getDirname from "../debug/getDirname.js";
-import testWiring from "./testWiring.js";
+import { writeFileSync } from "fs";
+import { getMinDiffDictCombinations } from "../combinationsOptimization/combinationsOptimization.js";
+import compareDataToFile from "../debug/compareDataToFile.js";
+import { getMinDiffDictPermutations } from "../permutationsOptimization/permutationsOptimization.js";
+import mapValues from "../utils/mapValues.js";
+import formatWiring from "./formatWiring.js";
 
 const DIRNAME = getDirname(import.meta.url);
 
-testWiring(
-    {
-        aux: {
-            m: "blue",
-            r: "red",
-            l: "green",
-            g: "copper"
-        },
-        sound: {
-            r: "red",
-            g: "copper",
-            l: "green",
-        }
-    },
-    join(DIRNAME, 'wiring.txt'),
-    join(DIRNAME, 'wiring copy.txt'),
-);
+const outputFile = join(DIRNAME, 'wiring.txt');
+const outputCompareFile = join(DIRNAME, 'wiring copy.txt');
+const wiringSettings = {
+    aux: { m: "blue", r: "red", l: "green", g: "copper" },
+    sound: { r: "red", g: "copper", l: "green" }
+};
+
+const wiringCombinations = getMinDiffDictCombinations(mapValues(wiringSettings, getMinDiffDictPermutations));
+const formattedCombinations = formatWiring(wiringCombinations);
+
+writeFileSync(outputFile, formattedCombinations);
+console.log(`result: "${outputFile}"`);
+
+if (outputCompareFile) compareDataToFile(formattedCombinations, outputCompareFile);
