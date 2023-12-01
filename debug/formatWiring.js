0a4a8db3208685d608fa847dbafb5789c27e2f64
Pavle <p.devidze@gmail.com>
2023-12-01 11:06:39 +0400

    refactor formatWiring jsdoc

--- debug/formatWiring.js
+++ debug/formatWiring.js
@@ -2,8 +2,14 @@ import indent from "../utils/indent.js";
 import lines from "../utils/lines.js";
 import map from "../utils/map.js";
 
+/** @typedef {string} Wire */
+/** @typedef {string} Joint */
+/** @typedef {Record<Joint, Wire>} Wiring */
+/** @typedef {string} WiringName */
+/** @typedef {Record<WiringName, Wiring>} Comb */
+
 /**
- * @param {Record<string, Record<string, string>>[]} combList 
+ * @param {Comb[]} combList 
  * @returns {string}
  */
 export default function formatWiring(combList) {
@@ -14,50 +20,50 @@ export default function formatWiring(combList) {
 }
 
 /**
- * @param {Record<string, Record<string, string>>} comb 
+ * @param {Comb} comb 
  * @param {number} index 
- * @param {Record<string, Record<string, string>>[]} combList 
+ * @param {Comb[]} combList 
  * @returns {string}
  */
 function formatCombinationEntry(comb, index, combList) {
     const nextComb = combList[index + 1];
-    const wiringSegments = map(comb, (wires, wiringName) =>
-        formatWiringSegment(wires, wiringName, nextComb)
+    const wiringSegments = map(comb, (wiring, wiringName) =>
+        formatWiringSegment(wiring, wiringName, nextComb)
     );
     return lines(`#${index + 1}:`, indent(lines(...wiringSegments)));
 }
 
 /**
- * @param {Record<string, string>} wires 
- * @param {string} wiringName 
- * @param {Record<string, Record<string, string>>} nextComb 
+ * @param {Wiring} wiring 
+ * @param {WiringName} wiringName 
+ * @param {Comb} nextComb 
  * @returns {string}
  */
-function formatWiringSegment(wires, wiringName, nextComb) {
-    const nextWires = nextComb?.[wiringName];
-    const wiringLines = map(wires, (wire, joint) =>
-        formatWireLine(wire, joint, nextWires)
+function formatWiringSegment(wiring, wiringName, nextComb) {
+    const nextWiring = nextComb?.[wiringName];
+    const wiringLines = map(wiring, (wire, joint) =>
+        formatWireLine(wire, joint, nextWiring)
     );
     return lines(`${wiringName}:`, indent(lines(...wiringLines)));
 }
 
 /**
- * @param {string} wire
- * @param {string} joint
- * @param {Record<string, string>} nextWires
+ * @param {Wire} wire
+ * @param {Joint} joint
+ * @param {Wiring} nextWiring
  * @returns {string}
  */
-function formatWireLine(wire, joint, nextWires) {
-    return `${joint} - ${wire}` + getNextWireSuffix(wire, joint, nextWires);
+function formatWireLine(wire, joint, nextWiring) {
+    return `${joint} - ${wire}` + getNextWireSuffix(wire, joint, nextWiring);
 }
 
 /**
- * @param {string} wire
- * @param {string} joint
- * @param {Record<string, string>} nextWires
+ * @param {Wire} wire
+ * @param {Joint} joint
+ * @param {Wiring} nextWiring
  */
-function getNextWireSuffix(wire, joint, nextWires) {
-    const nextWire = nextWires?.[joint];
+function getNextWireSuffix(wire, joint, nextWiring) {
+    const nextWire = nextWiring?.[joint];
     const wireChanges = nextWire && wire !== nextWire;
     return wireChanges ? ` -> ${nextWire}` : '';
 }
