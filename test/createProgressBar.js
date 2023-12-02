abe9f800ad0ad36823c3286a30ffb818316cb60f
Pavle <p.devidze@gmail.com>
2023-12-02 13:46:23 +0400

    update progress tests extract simulateProgress

--- test/createProgressBar.js
+++ test/createProgressBar.js
@@ -1,30 +1,7 @@
-import createProgressBar from "../debug/createProgressBar.js";
+import simulateProgress from "../debug/simulateProgress.js";
 
-testProgressBar();
-
-function testProgressBar() {
-    const totalSteps = 20;
-    const progressBarWidth = 20;
-    const progressBar = createProgressBar(totalSteps, progressBarWidth);
-
-    console.log('Simulating progress:');
-    simulateProgress(progressBar, totalSteps, 50);
-}
-
-/**
- * @param {{ increment: (...args: unknown[]) => void; }} progressBar
- * @param {number} totalSteps
- * @param {number} interval
- */
-function simulateProgress(progressBar, totalSteps, interval) {
-    let currentStep = 0;
-
-    const progressInterval = setInterval(() => {
-        if (currentStep < totalSteps) {
-            progressBar.increment();
-            currentStep++;
-        } else {
-            clearInterval(progressInterval);
-        }
-    }, interval);
-}
+(async () => {
+    console.log("Simulating progress...");
+    await simulateProgress(20, 20, 50);
+    console.log('Done');
+})();
--- test/createProgressBar.overflow.js
+++ test/createProgressBar.overflow.js
@@ -1,9 +1,8 @@
-import createProgressBar from "../debug/createProgressBar.js";
+import simulateProgress from "../debug/simulateProgress.js";
 
-const num = 20;
-
-const progress = createProgressBar(num, num);
-
-for (let i = 0; i < num; i++) progress.increment();
-
-progress.increment(); // NOTE: should print "Progress bar overflow!"
+(async () => {
+    console.log("Simulating progress...");
+    const progress = await simulateProgress(20, 20, 50);
+    console.log(`Should print: ${JSON.stringify("Progress bar overflow!")}`);
+    progress.increment();
+})();
