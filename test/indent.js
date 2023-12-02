33ea2aef28adf94406e35900c9776c911d387d14
Pavle <p.devidze@gmail.com>
2023-12-02 12:25:22 +0400

    update indent test add simpleAssert, fix input, refactor

--- test/indent.js
+++ test/indent.js
@@ -1,75 +1,77 @@
+import assert from "assert";
+import simpleAssert from "../debug/simpleAssert.js";
 import indent from "../utils/indent.js";
 
-const IDENT = '-';
+const IDENT = "-";
 
 test(
-  'easy',
+  "easy",
   "One\nTwo\nThree",
   "-One\n-Two\n-Three"
 );
 
 test(
-  'newline middle',
+  "newline middle",
   "One\n\nTwo\nThree",
   "-One\n\n-Two\n-Three"
 );
 
 test(
-  'double newline middle',
+  "double newline middle",
   "One\n\n\nTwo\nThree",
   "-One\n\n\n-Two\n-Three"
 );
 
 test(
-  'start newline',
+  "start newline",
   "\nOne\nTwo\nThree",
   "\n-One\n-Two\n-Three"
 );
 
 test(
-  'start newline whitespace',
+  "start newline whitespace",
   "    \nOne\nTwo\nThree",
   "    \n-One\n-Two\n-Three"
 );
 
 test(
-  'start newline whitespace newline',
-  "    \n\nOne\nTwo\nThree",
-  "    \n\n-One\n-Two\n-Three"
+  "start newline whitespace newline",
+  "\n    \nOne\nTwo\nThree",
+  "\n    \n-One\n-Two\n-Three"
 );
 
 test(
-  'start whitespace newline',
+  "start whitespace newline",
   "    \nOne\nTwo\nThree",
   "    \n-One\n-Two\n-Three"
 );
 
 test(
-  'end newline',
+  "end newline",
   "One\nTwo\nThree\n",
   "-One\n-Two\n-Three\n"
 );
 
 test(
-  'end newline whitespace',
-  "One\nTwo\nThree\n",
-  "-One\n-Two\n-Three\n"
+  "end newline whitespace",
+  "One\nTwo\nThree\n    ",
+  "-One\n-Two\n-Three\n    "
 );
 
 test(
-  'end newline whitespace newline',
+  "end newline whitespace newline",
   "One\nTwo\nThree\n    \n",
   "-One\n-Two\n-Three\n    \n"
 );
 
 test(
-  'end whitespace newline',
+  "end whitespace newline",
   "One\nTwo\nThree    \n",
   "-One\n-Two\n-Three    \n"
 );
 
 test(
-  'start lines whitespace',
+  "start lines whitespace",
   "  One\n  Two\n  Three",
   "-  One\n-  Two\n-  Three"
 );
@@ -82,8 +84,5 @@ test(
  */
 function test(description, input, expected) {
   const result = indent(input, IDENT);
-  if (result === expected)
-    console.log(`PASS: ${description}`);
-  else
-    console.error(`FAIL: ${description}. Expected ${JSON.stringify(expected)}, Got ${JSON.stringify(result)}`);
+  simpleAssert(() => assert.strictEqual(result, expected), description);
 }
