68963c56255a9558cb043f1d65c0a13403597455
Pavle <p.devidze@gmail.com>
2023-12-02 14:36:13 +0400

    update progressBar test merge progressBar overflow capture bar stdout

--- test/createProgressBar.js
+++ test/createProgressBar.js
@@ -1,8 +1,41 @@
+import assert from "assert";
+import captureConsoleAsync from "../debug/captureConsoleAsync.js";
 import simpleAssert from "../debug/simpleAssert.js";
 import simulateProgress from "../debug/simulateProgress.js";
+import normalizeEOL from "../utils/normalizeEOL.js";
+
+const total = 20;
+const width = 20;
+const interval = 50;
+const expected = `
+Progress: [=                   ]
+Progress: [==                  ]
+Progress: [===                 ]
+Progress: [====                ]
+Progress: [=====               ]
+Progress: [======              ]
+Progress: [=======             ]
+Progress: [========            ]
+Progress: [=========           ]
+Progress: [==========          ]
+Progress: [===========         ]
+Progress: [============        ]
+Progress: [=============       ]
+Progress: [==============      ]
+Progress: [===============     ]
+Progress: [================    ]
+Progress: [=================   ]
+Progress: [==================  ]
+Progress: [=================== ]
+Progress: [====================]
+Progress bar overflow!
+`;
 
 (async () => {
-    console.log("Simulating progress...");
-    await simulateProgress(20, 20, 50);
-    simpleAssert(() => { });
+    const captured = await captureConsoleAsync(async () => {
+        const progress = await simulateProgress(total, width, interval);
+        progress.increment();
+    });
+
+    simpleAssert(() => assert.deepStrictEqual(normalizeEOL(captured.join('')), expected));
 })();
