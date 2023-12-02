3be832e48127b651f3d91047184e6b142515f4d7
Pavle <p.devidze@gmail.com>
2023-12-02 10:28:25 +0400

    fix assertFileContent async

--- debug/assertFileContent.js
+++ debug/assertFileContent.js
@@ -7,7 +7,7 @@ import normalizeEOL from "../utils/normalizeEOL.js";
  * @param {string} expectedContent
  * @throws {Error}
  */
-export default async function assertFileContent(actualFile, expectedContent) {
+export default function assertFileContent(actualFile, expectedContent) {
     const actualContent = readFileSync(actualFile, 'utf8');
     const match = normalizeEOL(actualContent) === normalizeEOL(expectedContent);
     assert.ok(match, "File content does not match");
