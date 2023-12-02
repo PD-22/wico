e62d5114eb7fa1119f5564894a23cee1475e6d7f
Pavle <p.devidze@gmail.com>
2023-12-02 12:24:23 +0400

    add simpleAssert text

--- debug/simpleAssert.js
+++ debug/simpleAssert.js
@@ -1,13 +1,19 @@
 import { AssertionError } from "assert";
 
-/** @param {() => void} assertCallback */
-export default function simpleAssert(assertCallback) {
+/**
+ * @param {() => void} assertCallback
+ * @param {string} [text]
+ * @returns {void}
+ */
+export default function simpleAssert(assertCallback, text) {
+    const textAppend = text ? ': ' + text : '';
     try {
         assertCallback();
-        console.log('PASS');
+        console.log(`PASS${textAppend}`);
     } catch (error) {
         if (error instanceof AssertionError) {
-            console.error(`FAIL: ${error.message}`)
+            // TODO: add text
+            console.error(`FAIL${textAppend}: ${error.message}`)
         } else {
             console.error(error);
         }
