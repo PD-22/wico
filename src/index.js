0ab5ff1f70e7cfafc8faef7d93856ea955ff6b88
Pavle <p.devidze@gmail.com>
2023-12-04 15:17:11 +0400

    separate index and wiring test

--- src/index.js
+++ src/index.js
@@ -1,31 +1,21 @@
 import { existsSync, mkdirSync } from "fs";
 import { join } from "path";
-import assertFileContent from "../debug/assertFileContent.js";
-import simpleAssert from "../debug/simpleAssert.js";
 import { writeOutputToFile } from "../debug/testCombinatoricsPerformance.js";
 import mapValues from "../utils/mapValues.js";
 import { getMinDiffDictCombinations } from "./combinationsOptimization.js";
-import formatWiring from "./format.js";
+import formatWiring from "./formatWiring.js";
 import { getMinDiffDictPermutations } from "./permutationsOptimization.js";
 
 const DIRNAME = 'output';
 if (!existsSync(DIRNAME)) mkdirSync(DIRNAME);
-
 const outputFile = join(DIRNAME, 'wiring.txt');
-const outputCompareFile = join(DIRNAME, 'wiring-backup.txt');
-const wiringSettings = {
+
+const settings = {
     aux: { m: "blue", r: "red", l: "green", g: "copper" },
     sound: { r: "red", g: "copper", l: "green" }
 };
 
-const wiringPermutations = mapValues(wiringSettings, getMinDiffDictPermutations);
-const wiringCombinations = getMinDiffDictCombinations(wiringPermutations);
+const permutations = mapValues(settings, getMinDiffDictPermutations);
+const combinations = getMinDiffDictCombinations(permutations);
 
-const formattedCombinations = formatWiring(wiringCombinations);
-
-writeOutputToFile(formattedCombinations, outputFile);
-
-console.log(`Assert file Content "${outputCompareFile}"...`);
-existsSync(outputCompareFile) ?
-    simpleAssert(() => assertFileContent(outputCompareFile, formattedCombinations)) :
-    console.log("NOT FOUND");
+writeOutputToFile(formatWiring(combinations), outputFile);
---
+++ test/wiring.js
@@ -0,0 +1,31 @@
+import { existsSync, mkdirSync } from "fs";
+import { join } from "path";
+import assertFileContent from "../debug/assertFileContent.js";
+import simpleAssert from "../debug/simpleAssert.js";
+import { writeOutputToFile } from "../debug/testCombinatoricsPerformance.js";
+import { getMinDiffDictCombinations } from "../src/combinationsOptimization.js";
+import formatIndex from "../src/formatWiring.js";
+import { getMinDiffDictPermutations } from "../src/permutationsOptimization.js";
+import mapValues from "../utils/mapValues.js";
+
+const DIRNAME = 'output';
+if (!existsSync(DIRNAME)) mkdirSync(DIRNAME);
+const outputFile = join(DIRNAME, 'wiring.txt');
+const outputCompareFile = join(DIRNAME, 'wiring-backup.txt');
+
+const settings = {
+    aux: { m: "blue", r: "red", l: "green", g: "copper" },
+    sound: { r: "red", g: "copper", l: "green" }
+};
+
+const permutations = mapValues(settings, getMinDiffDictPermutations);
+const combinations = getMinDiffDictCombinations(permutations);
+
+const formatted = formatIndex(combinations);
+
+writeOutputToFile(formatted, outputFile);
+
+console.log(`Assert file Content "${outputCompareFile}"...`);
+existsSync(outputCompareFile) ?
+    simpleAssert(() => assertFileContent(outputCompareFile, formatted)) :
+    console.log("NOT FOUND");
