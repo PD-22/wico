23cfb30cbd265cab9744a9a56fa4d2cd6d933121
Pavle <p.devidze@gmail.com>
2023-12-07 23:51:35 +0400

    separate async captureConsole test

--- test/debug/captureConsole.js
+++ test/debug/captureConsole.js
@@ -1,46 +1,21 @@
 import assert from "assert";
 import captureConsole from "../../debug/captureConsole.js";
-import delay from "../../debug/delay.js";
 import simpleAssert from "../../debug/simpleAssert.js";
 
-(() => {
-    const captured = captureConsole(() => {
-        console.log("Hello, World!");
-        console.error("Error message");
-        console.info("Information message");
-        console.warn("Warning message");
-        process.stdout.write("Process stdout write");
-    });
+const actual = captureConsole(() => {
+    console.log("Hello, World!");
+    console.error("Error message");
+    console.info("Information message");
+    console.warn("Warning message");
+    process.stdout.write("Process stdout write");
+});
 
-    simpleAssert(
-        () => assert.deepStrictEqual(captured, [
-            "Hello, World!",
-            "Error message",
-            "Information message",
-            "Warning message",
-            "Process stdout write"
-        ]),
-        "synchronous"
-    );
-})();
+const expected = [
+    "Hello, World!",
+    "Error message",
+    "Information message",
+    "Warning message",
+    "Process stdout write"
+];
 
-(async () => {
-    const captured = await captureConsole(async () => {
-        await delay(50); console.log("Hello, World!");
-        await delay(50); console.error("Error message");
-        await delay(50); console.info("Information message");
-        await delay(50); console.warn("Warning message");
-        await delay(50); process.stdout.write("Process stdout write");
-    });
-
-    simpleAssert(
-        () => assert.deepStrictEqual(captured, [
-            "Hello, World!",
-            "Error message",
-            "Information message",
-            "Warning message",
-            "Process stdout write"
-        ]),
-        "asynchronous"
-    );
-})();
+simpleAssert(() => assert.deepStrictEqual(actual, expected));
