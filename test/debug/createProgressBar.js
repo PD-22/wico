3013a67da3d9e32ff5b9f0471ea9917fcaa2f1e3
Pavle <p.devidze@gmail.com>
2023-12-07 22:51:06 +0400

    remove simulateProgress

--- test/debug/createProgressBar.js
+++ test/debug/createProgressBar.js
@@ -1,12 +1,12 @@
 import assert from "assert";
 import captureConsole from "../../debug/captureConsole.js";
+import createProgressBar from "../../debug/createProgressBar.js";
 import normalizeEOL from "../../debug/normalizeEOL.js";
 import simpleAssert from "../../debug/simpleAssert.js";
-import simulateProgress from "../../debug/simulateProgress.js";
 
 const total = 5;
 const width = 10;
-const interval = 50;
+const delay = () => new Promise(resolve => setTimeout(resolve, 50));
 const expected = `
 Progress: [==        ]
 Progress: [====      ]
@@ -18,8 +18,13 @@ Progress bar overflow!
 
 (async () => {
     const captured = await captureConsole(async () => {
-        const progress = await simulateProgress(total, width, interval);
-        progress.increment();
+        const progress = createProgressBar(total, width);
+        await delay(); progress.increment();
+        await delay(); progress.increment();
+        await delay(); progress.increment();
+        await delay(); progress.increment();
+        await delay(); progress.increment();
+        await delay(); progress.increment();
     });
 
     simpleAssert(() => assert.deepStrictEqual(normalizeEOL(captured.join('')), expected));
