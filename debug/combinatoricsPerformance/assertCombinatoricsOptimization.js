6aec56e79cb517e397f6d2790bb1da2eaa50339d
Pavle <p.devidze@gmail.com>
2023-12-08 14:36:01 +0400

    add assertCombinatoricsOptimization errorMessage

--- debug/combinatoricsPerformance/assertCombinatoricsOptimization.js
+++ debug/combinatoricsPerformance/assertCombinatoricsOptimization.js
@@ -3,13 +3,18 @@ import countListDiff from "../../utils/countListDiff.js";
 import forEachAdjacents from "../../utils/forEachAdjacents.js";
 
 /**
- * @template T
- * @param {T[][][]} outputList
+ * @param {unknown[][][]} outputList
  * @param {number} expectedDiffCount
  * @throws {AssertionError}
  */
 export default function assertCombinatoricsOptimization(outputList, expectedDiffCount) {
-    outputList.forEach(output => forEachAdjacents(output, (v1, v2) =>
-        assert.strictEqual(countListDiff(v1, v2), expectedDiffCount)
-    ));
+    outputList.forEach((output, index) => forEachAdjacents(output, (v1, v2, i1, i2) => {
+        const diffCount = countListDiff(v1, v2);
+
+        const errorMessage = `Expected the difference between adjacent items in output (at position ${index}) to be ${expectedDiffCount}, but got ${diffCount}.\n` +
+            `Item 1: ${JSON.stringify(v1)} (at position ${i1})\n` +
+            `Item 2: ${JSON.stringify(v2)} (at position ${i2}).`;
+
+        assert.strictEqual(diffCount, expectedDiffCount, errorMessage);
+    }));
 }
