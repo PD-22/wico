e5ce98136a3e1202ea56e7aff52b6587a2ba2ade
Pavle <p.devidze@gmail.com>
2023-12-08 19:03:21 +0400

    WIP: add processCombinatorics test

---
+++ test/debug/combinatoricsPerformance/processCombinatorics.js
@@ -0,0 +1,46 @@
+import assert from 'assert';
+import captureConsole from '../../../debug/captureConsole.js';
+import processCombinatorics from '../../../debug/combinatoricsPerformance/processCombinatorics.js';
+import simpleAssert from '../../../debug/simpleAssert.js';
+import getMinDiffPermutations from '../../../src/permutationsOptimization.js';
+
+// TODO: i dont like this type, why tuple not working?
+/** @type {(number | string)[][]} */
+const input = [[0, 1, 2], ['A', 'B', 'C']];
+
+// TODO: captureConsole maybe should return the result as well
+/** @type {(string | number)[][][]} */
+let result;
+const logs = captureConsole(() => {
+    result = processCombinatorics(input, getMinDiffPermutations);
+});
+
+const expectedLogs = [
+    "getMinDiffPermutations...",
+    "\rProgress: [==========          ]",
+    "\rProgress: [====================]",
+    "\n",
+    // "0 ms",
+];
+
+const expectedResult = [
+    [
+        [0, 1, 2], [0, 2, 1], [1, 2, 0],
+        [1, 0, 2], [2, 0, 1], [2, 1, 0]
+    ],
+    [
+        ["A", "B", "C"], ["A", "C", "B"], ["B", "C", "A"],
+        ["B", "A", "C"], ["C", "A", "B"], ["C", "B", "A"]
+    ]
+];
+
+simpleAssert(() => {
+    assert.deepStrictEqual(result, expectedResult);
+
+    const lastIndex = logs.length - 1;
+    assert.deepStrictEqual(expectedLogs, logs.toSpliced(lastIndex));
+
+    const ms = String(logs[lastIndex]);
+    assert.match(ms, /\d+ ms/);
+    assert.ok(parseFloat(ms) >= 0, "ms should be greater than or equal to 0");
+});
